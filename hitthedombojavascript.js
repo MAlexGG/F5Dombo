5aaf4cef1f9546dd10eaa7474e8bb7288c562660
MAlexGG <alexandragalarzag@gmail.com>
2020-11-19 11:42:52 +0100

    versionAlex7

--- hitthedombojavascript.js
+++ hitthedombojavascript.js
@@ -3,6 +3,7 @@ document.addEventListener('click', function(event){
  
     const sonido = document.querySelector("audio[data-instrumento="+ instrumento +"]");
     sonido.play();
+    
     sonido.currentTime= 0;
 
     console.log(instrumento);
