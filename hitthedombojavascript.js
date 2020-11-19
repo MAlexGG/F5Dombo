d7a63a645a333e8e69508c2118f47a32bae8de07
Ferminrios <ferlucianorios@gmail.com>
2020-11-19 16:00:58 +0100

    VERSIONFERMIN19/11

--- hitthedombojavascript.js
+++ hitthedombojavascript.js
@@ -1,10 +1,12 @@
 document.addEventListener('click', function(event){
     let instrumento = event.target.dataset.instrumento;
+
+    sonido.currentTime= 0;
  
     const sonido = document.querySelector("audio[data-instrumento="+ instrumento +"]");
     sonido.play();
     
-    sonido.currentTime= 0;
+    
 
     console.log(instrumento);
  
