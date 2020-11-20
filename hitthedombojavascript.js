1e28050bb08567b5115b1c041a96a864e83b6361
Andreabcm <andreabcm16@gmail.com>
2020-11-20 10:48:02 +0100

    javascriptAndrea

--- hitthedombojavascript.js
+++ hitthedombojavascript.js
@@ -1,10 +1,19 @@
 document.addEventListener('click', function(event){
     let instrumento = event.target.dataset.instrumento;
+    let sonido = document.querySelector("audio[data-instrumento="+ instrumento +"]");
+    let animacion = document.getElementById(instrumento);
+    
 
+
+
+    animacion.classList.add("magicOlive");
+
+    animacion.classList.remove("magicOlive");
+    
     sonido.currentTime= 0;
- 
-    const sonido = document.querySelector("audio[data-instrumento="+ instrumento +"]");
     sonido.play();
+
+   
     
     
 
@@ -12,6 +21,8 @@ document.addEventListener('click', function(event){
  
     });
 
+
+        
 
 
 
